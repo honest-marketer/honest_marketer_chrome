ee7efc3bc92d14d2ad0f558efe07cac528ce97fb
Ryan Kulp <ryanckulp@gmail.com>
2019-08-28 22:09:46 +0200

    more offense

--- js/variables.js
+++ js/variables.js
@@ -2,7 +2,7 @@ console.log('running Honest Marketer extension...');
 
 var elements = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a, span"); // TODO: support 'div'
 
-var negativeWords = ['!', '#1', 'instant', 'best', 'superior', 'the future', 'easiest', 'fastest', 'most popular', 'most powerful'];
+var negativeWords = ['!', '#1', "world's leading", 'industry leading', 'instant', 'best', 'superior', 'the future', 'easiest', 'fastest', 'most popular', 'most powerful'];
 var neutralWords = ['everything you need', 'absolute', 'easy', 'fast', 'love', 'better', 'limited time', 'new way', 'powerful', 'popular'];
 var positiveWords = [' rate', 'first', 'patent', 'trademark', 'reviews', 'refund', 'free'];
 
